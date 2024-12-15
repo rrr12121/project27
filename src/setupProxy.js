e8b386f93adcd967d51ed7af342a19d1a3f7782a
7AICAT <rrr.fsdf@yandex.com>
2024-12-15 20:18:37 +0100

    Update proxy configuration and deployment setup

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,6 +6,20 @@ module.exports = function(app) {
     createProxyMiddleware({
       target: 'http://localhost:3001',
       changeOrigin: true,
+      pathRewrite: {
+        '^/api': '/api'
+      },
+      onProxyReq: function(proxyReq, req, res) {
+        // Log proxy requests
+        console.log('Proxying:', req.method, req.path, '->', proxyReq.path);
+      },
+      onError: function(err, req, res) {
+        console.error('Proxy error:', err);
+        res.writeHead(500, {
+          'Content-Type': 'text/plain',
+        });
+        res.end('Proxy error: ' + err.message);
+      }
     })
   );
 };
